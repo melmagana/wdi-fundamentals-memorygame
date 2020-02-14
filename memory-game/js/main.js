5ec482798285438ff9af5aad4095e42542f7b6f8
Melissa Magaña <melissamagana@melissas-mbp.attlocal.net>
2020-02-14 00:48:38 -0600

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+let cardOne = "Queen of Diamonds"
+let cardTwo = "King of Diamonds"
+let cardThree = "Queen of Hearts"
+let cardFour = "King of Hearts"
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
